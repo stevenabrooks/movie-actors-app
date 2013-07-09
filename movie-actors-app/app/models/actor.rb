e10bdda69bc7134d08551a3a1a4fc9eb44fff3a0
Dan Friedman <danielfriedman1@gmail.com>
2013-07-09 14:22:31 -0400

    add models n resources

--- movie-actors-app/app/models/actor.rb
+++ movie-actors-app/app/models/actor.rb
@@ -1,3 +1,5 @@
 class Actor < ActiveRecord::Base
   attr_accessible :movie_id, :name
+
+  belongs_to :movie
 end
--- movie-actors-app/app/models/movie.rb
+++ movie-actors-app/app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :actors
 end
