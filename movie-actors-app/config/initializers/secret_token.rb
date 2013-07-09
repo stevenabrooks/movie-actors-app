7829560690f9791ffbca1779f090e20adbac9afc
Dan Friedman <danielfriedman1@gmail.com>
2013-07-09 14:06:50 -0400

    first

---
+++ movie-actors-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieActorsApp::Application.config.secret_token = '1029bf334c5cf74328c1a29733165f8ef4d31892d813b46b2bf785f6bb0991a0b36260b43fa6624bb05b5eed20a20aa031e8c421e271bbfed4033a27d6383826'
