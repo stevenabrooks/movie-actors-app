1ae99a2b2498ad1d1f8a5ce452a7efa08de6b819
Dan Friedman <danielfriedman1@gmail.com>
2013-07-09 14:44:32 -0400

    add index view, route stubs

--- movie-actors-app/app/controllers/movies_controller.rb
+++ movie-actors-app/app/controllers/movies_controller.rb
@@ -1,2 +1,26 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
--- movie-actors-app/config/routes.rb
+++ movie-actors-app/config/routes.rb
@@ -4,6 +4,8 @@ MovieActorsApp::Application.routes.draw do
 
   resources :movies
 
+  
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
